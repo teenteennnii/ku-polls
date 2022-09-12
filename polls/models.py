0bcbaf5dd23522580e82aa525f2c6bfca2b09c16
teenteennnii <teennopwijit@gmail.com>
2022-09-12 19:27:58 +0700

    implement Question class

--- polls/models.py
+++ polls/models.py
@@ -7,10 +7,7 @@ from django.contrib import admin
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    end_date = models.DateTimeField('date ended', null=True)
     
     def __str__(self):
         return self.question_text
@@ -21,8 +18,19 @@ class Question(models.Model):
         description='Published recently?',
     )
     def was_published_recently(self):
+        """Check whether question was publish less than 1 day or not."""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    def is_published(self):
+        """Check current date is on or after question's publication date."""
+        return self.pub_date < timezone.localtime()
+    
+    def can_vote(self):
+        """Check if voting is allowed for this question."""
+        if self.end_date:  # check end_date is not NULL
+            return self.end_date > timezone.localtime()
+        return self.is_published()
 
 
 class Choice(models.Model):
