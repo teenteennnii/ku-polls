0d9fe970672c7834d80243aa982c84b9110d12b0
teenteennnii <teennopwijit@gmail.com>
2022-09-12 19:01:43 +0700

    fix missing function

--- polls/models.py
+++ polls/models.py
@@ -12,6 +12,9 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
+    def __str__(self):
+        return self.question_text
+    
     @admin.display(
         boolean=True,
         ordering='pub_date',
@@ -26,3 +29,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
