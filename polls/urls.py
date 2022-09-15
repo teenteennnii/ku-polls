116ee795fa17e1a569722d50133ce353b4d17227
teenteennnii <teennopwijit@gmail.com>
2022-09-15 21:15:08 +0700

    fix

--- polls/urls.py
+++ polls/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('signup/', views.signup, name='signup')
 ]
